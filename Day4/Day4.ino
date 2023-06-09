8c3ed019c62dc29ecd97c6f4a2cc7d8751a759f3
Endizen <theendizen@gmail.com>
2023-06-09 13:47:43 -0700

    Complete module 4.

--- Day4/Day4.ino
+++ Day4/Day4.ino
@@ -1,27 +1,51 @@
-int LED = 12;
-int DIP1 = 2;
-int DIP2 = 3;
-int DIP3 = 4;
+int LED1 = 12;
+int LED2 = 11;
+int LED3 = 10;
+int LED4 = 9;
 
+int DIP1 = 3;
+int DIP2 = 2;
+int DIP3 = 1;
+int DIP4 = 0;
 
 void setup() {
-  pinMode(LED, OUTPUT);
-  pinMode(DIP, INPUT);
+  pinMode(LED1, OUTPUT);
+  pinMode(LED2, OUTPUT);
+  pinMode(LED3, OUTPUT);
+  pinMode(LED4, OUTPUT);
+  pinMode(DIP1, INPUT);
+  pinMode(DIP2, INPUT);
+  pinMode(DIP3, INPUT);
+  pinMode(DIP4, INPUT);
 }
 
+// Problem is that this code is sequential, will flash at different times depending on when the switch is turned on
 void loop() {
-  if (digitalRead(DIP) == HIGH) {
-    digitalWrite(LED, LOW); // turn LED on
-    delay(1000);
-    digitalWrite(LED, HIGH);
-    delay(100);
-    digitalWrite(LED, LOW);
-    delay(100);
-    digitalWrite(LED, HIGH);
-    delay(100);
+  if (digitalRead(DIP1) == HIGH) {
+    digitalWrite(LED1, HIGH);
   }
   else {
-    digitalWrite(LED, LOW); // turn LED off
+    digitalWrite(LED1, LOW); // turn LED off
   }
 
+  if (digitalRead(DIP2) == HIGH) {
+    digitalWrite(LED2, HIGH);
+  }
+  else {
+    digitalWrite(LED2, LOW); // turn LED off
+  }
+  
+  if (digitalRead(DIP3) == HIGH) {
+    digitalWrite(LED3, HIGH);
+  }
+  else {
+    digitalWrite(LED3, LOW); // turn LED off
+  }
+  
+  if (digitalRead(DIP4) == HIGH) {
+    digitalWrite(LED4, HIGH);
+  }
+  else {
+    digitalWrite(LED4, LOW); // turn LED off
+  }
 }
