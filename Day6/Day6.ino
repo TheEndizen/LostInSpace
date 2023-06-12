7b4157ebb10aa7444fa0086e0c5a2f72fa938bf0
Endizen <theendizen@gmail.com>
2023-06-12 00:14:48 -0700

    Add part 1 code.

--- Day6/Day6.ino
+++ Day6/Day6.ino
@@ -1,7 +1,18 @@
+int sensorPin = A0;
+int sensorValue = 0;
+
+int onboardLED = 13;
+
 void setup() {
   Serial.begin(9600);
+  Serial.write("photoresistor data");
+  delay(1000);
+  pinMode(onboardLED, OUTPUT);
 }
 
 void loop() {
-  Serial.write("photoresistor data");
+  sensorValue = analogRead(sensorPin);
+  Serial.println(sensorValue);
+  delay(1000);
+
 }
