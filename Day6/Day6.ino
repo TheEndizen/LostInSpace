356534ce307ca13b127bb6cc48d294775f1a26be
Endizen <theendizen@gmail.com>
2023-06-12 22:35:06 -0700

    Add updated part 2 code.

--- Day6/Day6.ino
+++ Day6/Day6.ino
@@ -1,14 +1,11 @@
 int sensorPin = A0;
 int sensorValue = 0;
-
-// default pin for LED_BUILTIN on Arduino UNO
-int onboardLED = 13;
-
+int onboardPin = 13;
+// default pin 13 is used for LED_BUILTIN on Arduino UNO
 void setup() {
   Serial.begin(9600);
   Serial.write("photoresistor data");
-  delay(1000);
-  pinMode(onboardLED, OUTPUT);
+  pinMode(onboardPin, OUTPUT);
 }
 
 void loop() {
@@ -17,11 +14,12 @@ void loop() {
   // blink LED depending on resistance
   // high resistance when bright => slower blinks
   // low resistance when dark => quicker blinks
-  digitalWrite(onboardLED, HIGH);
+  digitalWrite(onboardPin, HIGH);
   delay(sensorValue);
-  digitalWrite(onboardLED, LOW);
+  digitalWrite(onboardPin, LOW);
   delay(sensorValue);
 
   // print data
   Serial.println(sensorValue);
+  delay(1000);
 }
