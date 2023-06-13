8966e27dc7dc45cb7d7200ecbefebf51f667ac7d
Endizen <theendizen@gmail.com>
2023-06-13 01:59:57 -0700

    Complete environment setup.

---
+++ Day9/Day9.ino
@@ -0,0 +1,25 @@
+// day 6
+int sensorPin = A0;
+int sensorValue = 0;
+ 
+//day 7
+unsigned int batteryCapacity = 50000;
+unsigned int batteryLevel = 0;
+unsigned int ticks = 0;
+unsigned int wait = 100;
+double percentCharged;
+
+// day 8
+int red = 11;
+int green = 10;
+int blue = 9;
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
