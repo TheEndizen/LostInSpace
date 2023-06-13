978fab92e068924de1e321814ca25822251838ae
Endizen <theendizen@gmail.com>
2023-06-13 13:50:51 -0700

    Update pwm colors.

--- Day8/Day8.ino
+++ Day8/Day8.ino
@@ -12,8 +12,12 @@ void setup() {
 
 void rgbColour(int redVal, int greenVal, int blueVal) {
   // introduction to pulse width modulation (PWM) -> turning a digital output on and off for a specified duration
-  // technique to produce analog-like signals with digital pins to be sent to components
+  // technique to produce analog results with digital means
+  // we cannot change the level of voltage but we can change the duration of the digital output (essentially analogWrite() for the digital pin)
+
   // recall rgb values range from 0 to 255
+  // pulse is the "on time"
+  // see https://docs.arduino.cc/learn/microcontrollers/analog-output
   analogWrite(red, redVal);
   analogWrite(green, greenVal);
   analogWrite(blue, blueVal);
@@ -22,15 +26,17 @@ void rgbColour(int redVal, int greenVal, int blueVal) {
 
 void loop() {
   // rotate through a bunch of colors
-  rgbColour(125, 255, 255); // Red
+  rgbColour(255, 0, 0); // red
   delay(1000);
-  rgbColour(255, 125, 255); // Green
+  rgbColour(0, 255, 0); // green
   delay(1000);
-  rgbColour(255, 255, 125); // Blue
+  rgbColour(0, 0, 255); // blue
   delay(1000);
-  rgbColour(125, 125, 255); // yellow
+  rgbColour(125, 125, 0); // yellow
   delay(1000);
-  rgbColour(125, 255, 125); // purple
+  rgbColour(125, 0, 125); // purple
+  delay(1000);
+  rgbColour(0, 125, 125); // cyan
   delay(1000);
   rgbColour(125, 125, 125); // white
   delay(1000);
