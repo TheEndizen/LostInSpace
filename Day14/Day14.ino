c77fe0fdda21537872edbd018ff14853cee9490d
Endizen <theendizen@gmail.com>
2023-07-03 22:52:12 -0700

    Add audiovisual effects.

--- Day14/Day14.ino
+++ Day14/Day14.ino
@@ -1,12 +1,12 @@
 #include <Key.h>
 #include <Keypad.h>
 
-// byte data type is equivalent to unsigned char on compiler
+// byte data type is equivalent to size of unsigned char on compiler
 const byte ROWS = 4;
 const byte COLS = 4;
 
 byte rowPins[ROWS] = {5, 4, 3, 2};
-byte colPins[COLS] = {6, 7, 8, 9};
+byte colPins[COLS] = {6, 7, 12, 13};
 
 char buttons[ROWS][COLS] = {
   {'1', '2', '3', 'A'},
@@ -21,6 +21,15 @@ Keypad matrix = Keypad(makeKeymap(buttons), rowPins, colPins, ROWS, COLS); // li
 const byte passLength = 4;
 char password[passLength] = {'0','0','0','0'};
 
+
+// add visual feedback (pwm led pins)
+int redPin = 11;
+int greenPin = 10;
+int bluePin = 9;
+
+// add audio feedback (piezo output pins)
+int buzzer = 8;
+
 void passReset() {
   Serial.println("Please enter a new password: ");
 
@@ -29,9 +38,11 @@ void passReset() {
     while(  !( input = matrix.getKey() )  ) {
       // wait indefinitely for keypad input of any kind
     }
+    playInput();
     password[i] = input;
     Serial.print("*");
   }
+  playSuccess();
   Serial.println("");
   Serial.println("Password changed successfully.");
   setup();
@@ -44,18 +55,60 @@ void passEnter() {
     while (  !( input = matrix.getKey() )  ) {
       // wait for an input
     }
+    playInput();
     Serial.print("*");
     if (input != password[i]) {
+      playError();
       Serial.println("");
       Serial.println("Incorrect password.");
+      setup();
       return; // stop as soon as the password is incorrect
     }
   }
+  playSuccess();
   Serial.println("");
   Serial.println("Correct password.");
 }
 
+// custom color using pwm
+void rgbColour(int redVal, int greenVal, int blueVal) {
+  analogWrite(redPin, redVal);
+  analogWrite(greenPin, greenVal);
+  analogWrite(bluePin, blueVal);
+}
+
+void playInput() {
+  tone(buzzer, 880, 200);
+  delay(50);
+  noTone(buzzer);
+}
+
+void playError() {
+  rgbColour(125, 0, 0);
+  tone(buzzer, 147, 200);
+  delay(100);
+  noTone(buzzer);
+  rgbColour(0, 0, 0);
+}
+
+void playSuccess() {
+  rgbColour(0, 125, 0);
+  tone(buzzer, 1000, 200);
+  delay(200);
+  tone(buzzer, 2700, 1000);
+  delay(1000);
+  noTone(buzzer);
+  rgbColour(0, 0, 0);
+}
+
+
 void setup() {
+  pinMode(redPin, OUTPUT);
+  pinMode(greenPin, OUTPUT);
+  pinMode(bluePin, OUTPUT);
+  rgbColour(0, 0, 0);
+
+
   Serial.begin(9600);
   delay(2000);
   
@@ -66,7 +119,8 @@ void setup() {
 
 void loop() {
   char input = matrix.getKey();
-
+  playInput();
+  
   if (input == '*') {
     delay(500);
     passReset();
