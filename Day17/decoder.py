3019aa10825492a0bb3b8a55ae85db81aae463b3
Endizen <theendizen@gmail.com>
2023-07-11 16:32:29 -0700

    Fixing issue with missing lines and incorrectly deciphering characters.

--- Day17/decoder.py
+++ Day17/decoder.py
@@ -1,23 +1,22 @@
 # translated "define" to a seven-letter shift
 # align the decoder string with the alphabet
-alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-upper = ""
+alphabet = "abcdefghijklmnopqrstuvwxyz"
+upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 # precondition: lineData is a string of characters
 
 # file reader
 file = open("Day17.ino")
-lineData = file.readline()
+lineData = ""
 
 for line in file:
     for character in alphabet:
         #print(character)
         if character.islower():
             pos = alphabet.find(character)
-            lineData = lineData.replace(character, alphabet[(pos-7)%26])
+            lineData = lineData.replace(character, alphabet[(pos+19)%26])
         elif character.isupper():
             pos = upper.find(character)
-            # ISSUE: need to modulus the entire string length for uppercase letters
-            lineData = lineData.replace(character, alphabet[(pos-7)%26+26])
+            lineData = lineData.replace(character, upper[pos+19]%26)
     print(lineData)
     lineData = file.readline()
