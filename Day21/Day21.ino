bc5ab30702a3c6a32ccf9517663bd970342707ae
Endizen <theendizen@gmail.com>
2023-07-11 17:34:11 -0700

    Add sample code.

--- Day21/Day21.ino
+++ Day21/Day21.ino
@@ -1,9 +1,28 @@
-void setup() {
-  // put your setup code here, to run once:
-
+#include <U8glib.h>
+ 
+ // particular board
+U8GLIB_SH1106_128X64  My_u8g_Panel(U8G_I2C_OPT_NONE); // I2C / TWI
+ 
+void draw(void) {
+  // graphic commands to redraw the complete screen should be placed here
+  // origin is at the upper left hand corner
+  My_u8g_Panel.setFont(u8g_font_unifont);
+  My_u8g_Panel.drawStr( 0, 10, "Build");
+  My_u8g_Panel.drawStr( 0, 30, "everything!");
+  My_u8g_Panel.drawStr( 0, 50, "Invent safe!!");
+ 
 }
-
-void loop() {
-  // put your main code here, to run repeatedly:
+ 
+void setup(void) {
+  // assign default color value
+  My_u8g_Panel.setColorIndex(1); // pixel on
+}
+ 
+void loop(void) {
+  // picture loop
+  My_u8g_Panel.firstPage();
+  do {
+    draw();
+  } while( My_u8g_Panel.nextPage() );
 
 }
